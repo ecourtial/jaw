ca3f408d22583aca3f82e85b628bb99b2ea44070
Eric COURTIAL <eric.courtial@wizacha.com>
2022-03-30 16:43:42 +0200

    Base of the user edition OK

--- src/Controller/Admin/ProfileController.php
+++ src/Controller/Admin/ProfileController.php
@@ -8,18 +8,39 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractAdminController
 {
     #[Route('/admin/profile', methods: ['GET', 'POST'], name: 'profile')]
-    public function __invoke(): Response
+    public function __invoke(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'user.updated_successfully');
+
+            return $this->redirectToRoute('profile');
+        }
+
+
         return $this->generateView(
             'admin/user/profile.html.twig',
             'My profile',
             "My profile",
+            [
+                'user' => $user,
+                'form' => $form->createView(),
+            ]
         );
     }
 }

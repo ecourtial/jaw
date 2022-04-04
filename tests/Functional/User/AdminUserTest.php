6567757f4d860eb61a8d6365a9b708ac7adb7fac
Eric COURTIAL <eric.courtial@wizacha.com>
2022-04-04 15:46:26 +0200

    WIP

---
+++ tests/Functional/User/AdminUserTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Factorize the code that could be used by other tests
+ *
+ * SCENARIO ADMIN:
+ * - Creates a user with the command (in the CI).
+ * - Connects to the admin with the credentials given to the command.
+ * - Changes email and username.
+ * - Read the token.
+ * - Change password
+ * - Logout.
+ * - Connects again with the new credentials.
+ * - Check that the token has not changed.
+ * - Try to access configuration panel: change all the values
+ * - Logout.
+ * - Connect again.
+ * - Check the configuration values: they have been properly updated
+ */
+
+
